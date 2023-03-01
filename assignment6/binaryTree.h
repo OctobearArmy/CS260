ee13ce04977285af83cdc671a6d5038e95bc07aa
OctobearArmy <sarah.mortensen.6499@mail.linnbenton.edu>
2023-02-28 23:11:13 -0800

    add comments

--- assignment6/binaryTree.h
+++ assignment6/binaryTree.h
@@ -9,6 +9,8 @@ using std::cout;
 using std::endl;
 using std::string;
 
+
+//============================================================================//
 template <typename T>
 class binaryTree {
   typedef BinaryNode<T> _N;
@@ -16,7 +18,7 @@ class binaryTree {
 private:
   _N *_root;
 
-  _N *_add(T val, _N *cur) { //log(n)
+  _N *_add(T val, _N *cur) { //O(logn) if balanced otherwise worst case scenerio O(n)
     if (cur) {
       if (val <= cur->data) cur->left  =_add(val, cur->left);
       else                  cur->right =_add(val, cur->right);
@@ -24,14 +26,16 @@ private:
     }
     return new _N(val);
   }
+//----------------------------------------------------------------------------//
 
-  void _delete(_N *cur) { //top down deletion log(n)
+  void _delete(_N *cur) { //top down deletion 
     if (cur) {
        _delete(cur->left);
        _delete(cur->right);
        delete cur; 
     }
   }
+//----------------------------------------------------------------------------//
 
   void _printInOrder(_N *cur, string prefix) { //O(n)
     if (cur) {
@@ -40,6 +44,7 @@ private:
       _printInOrder(cur->right, prefix);
     }
   }
+//----------------------------------------------------------------------------//
 
   void _printPreOrder(_N *cur) { //O(n)
     if (cur) {
@@ -48,6 +53,7 @@ private:
       _printPreOrder(cur->right);
     }
   }
+//----------------------------------------------------------------------------//
 
   // O(n)
   // Backwards post order traversal to build a new binary tree where 
@@ -68,6 +74,7 @@ private:
     down =_printLevelOrder(cur->left , down);
     return new _N(cur->data, level, down);
   }
+//----------------------------------------------------------------------------//
 
   // int _max(T a, T b) { return a > b ? a : b; }
 
@@ -75,9 +82,9 @@ private:
   //   if (cur) return _max(_height(cur->left), _height(cur->right)) + 1;
   //   else     return -1;
   // }
+//----------------------------------------------------------------------------//
 
-
-  _N **_find(T val, _N **cur) { //logn()
+  _N **_find(T val, _N **cur) { //O(logn) if balanced otherwise worst case scenerio O(n)
     if (cur && *cur) {
       T data = (*cur)->data;
       if (val == data) return cur;
@@ -86,18 +93,21 @@ private:
     }
     return nullptr;
   }
+//----------------------------------------------------------------------------//
 
-  _N **_findMin(_N **cur) { //logn()
+  _N **_findMin(_N **cur) { //O(logn) if balanced otherwise worst case scenerio O(n)
     while ((*cur)->left) cur = &((*cur)->left);
     return cur;
   }
+//----------------------------------------------------------------------------//
 
-  _N **_findMax(_N **cur) { //logn()
+  _N **_findMax(_N **cur) { //O(logn) if balanced otherwise worst case scenerio O(n)
     while ((*cur)->right) cur = &((*cur)->right);
     return cur;
   }
+//----------------------------------------------------------------------------//
 
-  void _deleteNode(_N **cur) { //logn()
+  void _deleteNode(_N **cur) { //O(logn) if balanced
     if (!(*cur)->left && !(*cur)->right) {
       delete *cur;
       *cur = nullptr;
@@ -108,30 +118,33 @@ private:
     (*cur)->data = (*node)->data;
     _deleteNode(node);
   }
-
+//----------------------------------------------------------------------------//
 
 public:
   binaryTree() {_root = nullptr; }
 
-  void addValue(T val) {_root =_add(val,_root); } //logn()
+  void addValue(T val) {_root =_add(val,_root); } //Ologn()
 
-  void deleteValue(T val) {_deleteNode(_find(val, &_root)); } //logn()
+  void deleteValue(T val) {_deleteNode(_find(val, &_root)); } //Ologn()
   
   void printInOrder() {_printInOrder(_root, ""); } //O(n)
   
   void printPreOrder() {_printPreOrder(_root); } //O(n)
 
-//reverse pre or
+//----------------------------------------------------------------------------//
   void printLevelOrder() { //O(n)
     _N *tree =_printLevelOrder(_root);
 
     _printPreOrder(tree);
     _delete(tree);
   }
+//----------------------------------------------------------------------------//
 
   // int height() { return _height(_root); }
+  
 
   ~binaryTree() {_delete(_root); _root = nullptr; }
 
 };
+//============================================================================//
 
